f9e22270f1b6d7c1c5b92c62c2404a55c3144504
Will Taylor <w1ll100@hotmail.co.uk>
2021-11-16 19:43:14 +0000

    Remove routing I'm not planning to use

--- src/components/app.js
+++ src/components/app.js
@@ -1,20 +1,14 @@
 import { h } from 'preact';
-import { Router } from 'preact-router';
 
 import Header from './header';
 
 // Code-splitting is automated for `routes` directory
-import Home from '../routes/home';
-import Profile from '../routes/profile';
+import Home from './home';
 
 const App = () => (
 	<div id="app">
 		<Header />
-		<Router>
-			<Home path="/" />
-			<Profile path="/profile/" user="me" />
-			<Profile path="/profile/:user" />
-		</Router>
+        <Home />
 	</div>
 )
 

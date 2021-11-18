b87f225f144f352f28b696376eb982490642fc64
Will Taylor <w1ll100@hotmail.co.uk>
2021-11-18 20:29:39 +0000

    Highlight active tab

--- src/components/tab-window/index.js
+++ src/components/tab-window/index.js
@@ -7,10 +7,14 @@ const TabWindow = props => {
         <div>
             <div class={style.tabSwitcher}>
                 {props.children.map((c, i) =>
-                    <button onClick={() => setActiveTab(i)}>{i + 1}</button>)}
+                    <button
+                        onClick={() => setActiveTab(i)}
+                        class={i === activeTab ? style.selectedTab : null}>
+                        {i + 1}
+                    </button>)}
             </div>
             {props.children.map((c, i) =>
-                <div class={i === activeTab ? style.shown : style.hidden}>{c}</div>)}
+                <div class={i === activeTab ? null : style.hidden}>{c}</div>)}
         </div>
     );
 }

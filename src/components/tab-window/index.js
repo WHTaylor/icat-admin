9695d46beb537094182b31ebe2278e5e9db36a7a
Will Taylor <w1ll100@hotmail.co.uk>
2021-11-18 20:23:48 +0000

    Very basic tabs for tables

---
+++ src/components/tab-window/index.js
@@ -0,0 +1,18 @@
+import {useState} from "preact/hooks";
+import style from './style.css';
+
+const TabWindow = props => {
+    const [activeTab, setActiveTab] = useState(null);
+    return (
+        <div>
+            <div class={style.tabSwitcher}>
+                {props.children.map((c, i) =>
+                    <button onClick={() => setActiveTab(i)}>{i + 1}</button>)}
+            </div>
+            {props.children.map((c, i) =>
+                <div class={i === activeTab ? style.shown : style.hidden}>{c}</div>)}
+        </div>
+    );
+}
+
+export default TabWindow;
--- src/components/view-thing/index.js
+++ src/components/view-thing/index.js
@@ -1,24 +1,31 @@
-import {useEffect, useState} from "preact/hooks";
+import {useState} from "preact/hooks";
 import style from './style.css';
 
 import {entityNames} from '../../icat.js';
 import EntityTable from '../../components/entity-table/container';
+import TabWindow from '../../components/tab-window';
 
 const ViewThing = ({icatClient, sessionId}) => {
     const [selectedEntity, setSelectedEntity] = useState(null);
+    const [selectedEntities, setSelectedEntities] = useState([]);
+
+    const openTab = e => setSelectedEntities(selectedEntities.concat([e]));
+
     return (
         <div class={style.viewContainer}>
             <ul>
                 {entityNames.map(en =>
                     <li>
-                      <button onClick={() => setSelectedEntity(en)}>{en}</button>
+                      <button onClick={() => openTab(en)}>{en}</button>
                     </li>) }
             </ul>
-            { selectedEntity != null &&
-                <EntityTable
-                    icatClient={icatClient}
-                    sessionId={sessionId}
-                    table={selectedEntity} /> }
+            <TabWindow>
+                {selectedEntities.map(e =>
+                    <EntityTable
+                        icatClient={icatClient}
+                        sessionId={sessionId}
+                        table={e} />) }
+            </TabWindow>
         </div>
     );
 }

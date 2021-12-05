81b0babc8ce03f17f259306ca6bb2a890bf7ddf5
Will Taylor <w1ll100@hotmail.co.uk>
2021-12-05 22:15:01 +0000

    Autologin to last cached server if still valid

--- src/components/app.js
+++ src/components/app.js
@@ -5,6 +5,7 @@ import IcatClient from '../icat.js';
 import Header from './header';
 import EntityViewer from './entity-viewer';
 import ServerConnector from './server-connector';
+import {getLastLogin, saveLogin, invalidateLogin} from '../servercache.js';
 
 const App = () => {
     const [connections, setConnections] = useState([]);
@@ -15,11 +16,21 @@ const App = () => {
         setConnections(
             connections.concat({"server": server, "sessionId": sessionId}));
         setActiveServer(numConnections);
+        saveLogin(server, sessionId);
     };
 
     const disconnect = i => {
         setConnections(connections.slice(0, i).concat(connections.slice(i + 1)));
     };
+
+    useLayoutEffect(() => {
+        if (connections.length > 0) return;
+        const [server, sessionId] = getLastLogin();
+        if (server == null || sessionId === null) return;
+        const client = new IcatClient(server);
+        client.isValidSession(sessionId)
+            .then(res => {if (res) createConnection(server, sessionId)});
+    });
 
     return (
         <div id="app">

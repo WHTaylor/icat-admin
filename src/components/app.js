e6f970a075be5914ac5d85f1d1957e8a3de9ab56
Will Taylor <w1ll100@hotmail.co.uk>
2021-12-17 13:01:31 +0000

    Close servers with middle click
    Also simplifies active page logic, fixing some bugs with it in the process

--- src/components/app.js
+++ src/components/app.js
@@ -10,14 +10,18 @@ import {getLastLogin, saveLogin, invalidateLogin} from '../servercache.js';
 
 const App = () => {
     const [connections, setConnections] = useState([]);
-    const [activeServer, setActiveServer] = useState(null);
-    const [showAbout, setShowAbout] = useState(false);
+    // activePage is one of
+    //  - null - show a login page
+    //  - "about" - show the about page
+    //  - A number - show EntityViewer for the corresponding server
+    const [activePage, setActivePage] = useState(null);
 
+    const aServerIsActive = () => typeof activePage === "number";
     const createConnection = (server, sessionId) => {
         const numConnections = connections.length;
         setConnections(
             connections.concat({"server": server, "sessionId": sessionId}));
-        setActiveServer(numConnections);
+        setActivePage(numConnections);
         saveLogin(server, sessionId);
     };
 
@@ -25,14 +29,27 @@ const App = () => {
         const c = connections[i];
         invalidateLogin(c.server);
         new IcatClient(c.server, c.sessionId).logout();
-        if (i < activeServer) {
-            setActiveServer(i - 1);
-        } else if (i === activeServer) {
-            if (i === 0) setActiveServer(null);
-            else if (i === connections.length - 1) setActiveServer(activeServer - 1);
-        }
         setConnections(connections.slice(0, i).concat(connections.slice(i + 1)));
     };
+
+    const removeConnection = i => {
+        const numConnections = connections.length;
+
+        disconnect(i);
+
+        // Select correct page
+        if (!aServerIsActive) return;
+
+        if (i < activePage) {
+            setActivePage(activePage - 1);
+        } else if (i === activePage) {
+            if (i === 0) {
+                if (numConnections === 1) setActivePage(null);
+                else setActivePage(0);
+            }
+            else if (i === numConnections - 1) setActivePage(activePage - 1);
+        }
+    }
 
     useLayoutEffect(() => {
         if (connections.length > 0) return;
@@ -43,30 +60,22 @@ const App = () => {
             .then(res => {if (res) createConnection(server, sessionId)});
     });
 
-    useEffect(() => {
-        const logout = ev => {
-            if (ev.key === "q" && activeServer !== null) disconnect(activeServer);
-        };
-        document.addEventListener("keydown", logout);
-        return () => document.removeEventListener("keydown", logout);
-    });
-
     return (
         <div id="app">
             <Header
                 servers={connections}
-                activePage={showAbout ? "about" : activeServer}
-                setActiveServer={i => {setShowAbout(false); setActiveServer(i)}}
-                showAbout={() => setShowAbout(true)}
-                showLoginForm={() => {setShowAbout(false); setActiveServer(null)}} />
+                activePage={activePage}
+                setActiveServer={i => setActivePage(i)}
+                closeServer={removeConnection}
+                showAbout={() => setActivePage("about")}
+                showLoginForm={() => setActivePage(null)} />
             {connections.map((c, i) =>
                 <EntityViewer
                     key={c.sessionId}
                     icatClient={new IcatClient(c.server, c.sessionId)}
-                    visible={!showAbout && i === activeServer} />)}
-            {showAbout
-                ? <About />
-                : activeServer === null &&
+                    visible={aServerIsActive && i === activePage} />)}
+            {activePage === "about" && <About /> }
+            {activePage === null &&
                 <ServerConnector
                     createConnection={createConnection} />}
         </div>

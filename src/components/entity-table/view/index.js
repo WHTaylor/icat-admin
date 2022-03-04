c6a92d37da825cbed6763263a2a21b5c404b7ae1
Will Taylor <w1ll100@hotmail.co.uk>
2022-03-04 13:51:51 +0000

    Correct sort button directions

--- src/components/entity-table/view/index.js
+++ src/components/entity-table/view/index.js
@@ -167,17 +167,17 @@ const EntityTableView = ({
                                 {k}
                                 <span>
                                     <button
-                                        class={sortingBy.field === k && !sortingBy.asc
-                                            && style.activeSort}
-                                        onClick={() => toggleSortBy(k, false)}
-                                        title={`Sort by ${k}, descending`}>
-                                        ▲
-                                    </button>
-                                    <button
                                         class={sortingBy.field === k && sortingBy.asc
                                             && style.activeSort}
                                         onClick={() => toggleSortBy(k, true)}
                                         title={`Sort by ${k}, ascending`}>
+                                        ▲
+                                    </button>
+                                    <button
+                                        class={sortingBy.field === k && !sortingBy.asc
+                                            && style.activeSort}
+                                        onClick={() => toggleSortBy(k, false)}
+                                        title={`Sort by ${k}, descending`}>
                                         ▼
                                     </button>
                                 </span>

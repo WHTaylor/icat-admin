c6b30a7e06ce08dfee2888f0047e02b9580bbdb6
Will Taylor <w1ll100@hotmail.co.uk>
2022-09-25 00:10:51 +0100

    Split context-menu tests into separate file

--- cypress/e2e/prod.cy.js
+++ cypress/e2e/prod.cy.js
@@ -43,28 +43,6 @@ describe('Read more works', () => {
   })
 })
 
-describe('Open one-many related works', () => {
-  it('passes', () => {
-    cy.openEntityByTyping('Facility');
-    cy.get('[class*="entityRow"]').trigger('contextmenu');
-    cy.get('div[class*="contextMenu"]').contains('investigations').then(i => i.click());
-    // Close the facility tab so we can easily get the filter input of the new tab
-    cy.get('[class*="tabSwitcher"] button').first().trigger('mousedown', {buttons: 4});
-    cy.get('[class*="filterInput"]').should('have.value', "facility.id = 1");
-  })
-})
-
-describe('Open many-one related works', () => {
-  it('passes', () => {
-    cy.openEntityByTyping('Instrument');
-    cy.get('[class*="entityRow"]').first().trigger('contextmenu');
-    cy.get('div[class*="contextMenu"]').contains('facility').then(i => i.click());
-    // Close the instrument tab so we can easily get the filter input of the new tab
-    cy.get('[class*="tabSwitcher"] button').first().trigger('mousedown', {buttons: 4});
-    cy.get('[class*="filterInput"]').should('have.value', "id = 1");
-  })
-})
-
 // We don't test opening _all_ tables, because around the 80% mark they get quite slow
 describe('Can open many tables at once', () => {
   it('passes', () => {

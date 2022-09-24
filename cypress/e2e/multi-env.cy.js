d4fe847dcaec207330f63173f17a375c4ee7bd39
Will Taylor <w1ll100@hotmail.co.uk>
2022-09-25 00:06:31 +0100

    Make test stable

--- cypress/e2e/multi-env.cy.js
+++ cypress/e2e/multi-env.cy.js
@@ -1,17 +1,21 @@
 describe('Can login to both envs, and urls are saved', () => {
   it('passes', () => {
     cy.login('prod')
-    cy.contains('ICAT tables');
     cy.contains('+').click();
     cy.loginNewServer('dev');
-    cy.contains('ICAT tables');
     cy.get('[class*="active"]').should('include.text', 'icat-dev');
     cy.get('header nav a').first().click();
     cy.get('[class*="active"]').should('include.text', 'icatisis');
-
-    cy.get('header nav a').first().trigger('mousedown', {buttons: 4});
-    cy.get('header nav a').first().trigger('mousedown', {buttons: 4});
-    cy.get('header nav a').first().click();
+    // Close the two open server tabs
+    cy.get('header nav a').then(navLinks => {
+      const numLinks = navLinks.length;
+      cy.get('header nav a:first-child').trigger('mousedown', {buttons: 4})
+        .then(() => {
+          cy.get('header nav a:first-child').trigger('mousedown', {buttons: 4})
+            .then(() => cy.get('header nav a').should('have.length', numLinks -2));
+        });
+    })
+    cy.get('header nav a:first-child').click();
     cy.get('#serverInput option').should('have.length', 2);
   })
 })

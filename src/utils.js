7a3ffd25f2ed1632149d97b3dcb8fdbdf1dcee9f
Will Taylor <william.h.taylor@stfc.ac.uk>
2023-01-20 16:59:14 +0000

    Allow date updates without milliseconds

--- src/utils.js
+++ src/utils.js
@@ -121,7 +121,28 @@ export function difference(set, other) {
 const dateFormats = [
  "YYYY-MM-DDTHH:mm:ss.SSSZ",
  "YYYY-MM-DDTHH:mm:ss.SSS[Z]",
+ "YYYY-MM-DDTHH:mm:ssZ",
+ "YYYY-MM-DDTHH:mm:ss[Z]",
 ];
 export function parseISODate(s) {
     return dayjs(s, dateFormats, true);
+}
+
+/* Convert date formats to the single format that ICAT allows.
+ *
+ * This allows users to change dates without specifying milliseconds, which are
+ * truncated anyway
+ * */
+export function withCorrectedDateFormats(entity) {
+    return Object.fromEntries(
+        Object.entries(entity)
+            .map(([k, v]) => {
+                const asDate = parseISODate(v);
+                if (asDate.isValid()) {
+                    const formatted = asDate.format("YYYY-MM-DDTHH:mm:ss.000Z");
+                    return [k, formatted];
+                } else {
+                    return [k, v];
+                }
+            }));
 }

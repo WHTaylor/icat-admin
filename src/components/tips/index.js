724710086ea6c7c97f69942355543925ee29fe6b
Will Taylor <william.h.taylor@stfc.ac.uk>
2023-05-19 15:52:37 +0100

    Add logout tip

--- src/components/tips/index.js
+++ src/components/tips/index.js
@@ -10,6 +10,7 @@ const Tips = () => (
             <li>Alt-Shift-O opens a modal which can be used to open new entity tabs by typing the name of the entity.
             After starting to type, scroll through the list of suggestions with the arrow keys or page up/down.</li>
             <li>Middle click an entity tab to close it.</li>
+            <li>Middle click a connection tab in the top bar to logout and close it.</li>
             <li>Drag and drop entity tabs to change their order.</li>
             <li>The most recently opened tab will be restored on next visit, if the session from it is still valid.</li>
             <li>If an update fails, this marker will be displayed:

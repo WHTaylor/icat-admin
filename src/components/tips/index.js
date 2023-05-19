dd160b9f77a486ffb0d902ac34ad6bac69cf0864
Will Taylor <william.h.taylor@stfc.ac.uk>
2023-05-19 15:52:06 +0100

    Update tips with new method of opening tabs

--- src/components/tips/index.js
+++ src/components/tips/index.js
@@ -7,10 +7,8 @@ const Tips = () => (
         <h2>Tips</h2>
         <ul>
             <li>Right click an entry to get links to related entities. Selecting one will open a new entity tab.</li>
-            <li>Start typing an entity name and press enter to open a new tab for that entity type. Only works if the
-                main page body has focus, press escape to clear anything that has been typed so far. Autocompletes to
-                the entity name with the longest matching subsequence.
-            </li>
+            <li>Alt-Shift-O opens a modal which can be used to open new entity tabs by typing the name of the entity.
+            After starting to type, scroll through the list of suggestions with the arrow keys or page up/down.</li>
             <li>Middle click an entity tab to close it.</li>
             <li>Drag and drop entity tabs to change their order.</li>
             <li>The most recently opened tab will be restored on next visit, if the session from it is still valid.</li>

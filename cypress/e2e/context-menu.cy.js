37e0a803cc0b1023ea61d2d63b6f97318ccdcbb3
Will Taylor <william.h.taylor@stfc.ac.uk>
2023-07-05 17:02:41 +0100

    Avoid deprecated method

--- cypress/e2e/context-menu.cy.js
+++ cypress/e2e/context-menu.cy.js
@@ -7,7 +7,7 @@ describe('Open one-many related works', () => {
   it('passes', () => {
     cy.openEntityByTyping('Facility');
     cy.get('[class*="entityRow"]').trigger('contextmenu');
-    cy.get('div[class*="contextMenu"]').contains('investigations').then(i => i.click());
+    cy.get('div[class*="contextMenu"]').contains('investigations').click();
     // Close the facility tab so we can easily get the filter input of the new tab
     cy.get('[class*="tabSwitcher"] button').first().trigger('mousedown', {buttons: 4});
     cy.get('[class*="filterInput"]').should('have.value', "facility.id = 1");
@@ -18,7 +18,7 @@ describe('Open many-one related works', () => {
   it('passes', () => {
     cy.openEntityByTyping('Instrument');
     cy.get('[class*="entityRow"]').first().trigger('contextmenu');
-    cy.get('div[class*="contextMenu"]').contains('facility').then(i => i.click());
+    cy.get('div[class*="contextMenu"]').contains('facility').click();
     // Close the instrument tab so we can easily get the filter input of the new tab
     cy.get('[class*="tabSwitcher"] button').first().trigger('mousedown', {buttons: 4});
     cy.get('[class*="filterInput"]').should('have.value', "id = 1");

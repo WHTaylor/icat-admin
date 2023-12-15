f25147de69ae9131f5386aae0bc9a082ccd10ae0
Will Taylor <william.h.taylor@stfc.ac.uk>
2023-12-15 12:01:26 +0000

    Don't export function unnecessarily

--- src/utils.ts
+++ src/utils.ts
@@ -96,7 +96,7 @@ function capitalize(s: string): string {
     return s.charAt(0).toUpperCase() + s.slice(1);
 }
 
-export function lowercaseFirst(s: string): string {
+function lowercaseFirst(s: string): string {
     return s.charAt(0).toLowerCase() + s.slice(1);
 }
 

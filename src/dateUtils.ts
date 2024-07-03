WHTaylor/icat-admin

commit fe7fa2efcde9c854a4cbc173923f6ca2f927d189

Serialize boolean values when writing to ICAT
Fixes https://github.com/WHTaylor/icat-admin/issues/36

src/dateUtils.ts
@@ -1,5 +1,4 @@
 import dayjs from "dayjs";
-import {ExistingIcatEntity, NewIcatEntity} from "./types";
 
 // Date formats that can be used for creating/editing date fields
 // Dates received from ICAT will always be in the first format
@@ -18,27 +17,4 @@ export function parseDate(s: string) {
 
 export function inIcatFormat(dt: dayjs.Dayjs) {
     return dt.format("YYYY-MM-DDTHH:mm:ss.000Z");
-}
-
-/**
- * Formats all date strings on an object into the datetime format ICAT enforces
- *
- * This allows users to change dates without specifying milliseconds, which are
- * truncated anyway, or any time at all if midnight is reasonable (ie. cycles)
- */
-export function withCorrectedDateFormats(entity: ExistingIcatEntity | NewIcatEntity) {
-    return Object.fromEntries(
-        Object.entries(entity)
-            .map(([k, v]) => {
-                if (typeof v === "object" || typeof v === "number") {
-                    return [k, v];
-                }
-
-                const asDate = parseDate(v);
-                if (asDate.isValid()) {
-                    return [k, inIcatFormat(asDate)];
-                }
-
-                return [k, v];
-            }));
 }

src/components/entity-table/row/index.tsx
@@ -2,14 +2,13 @@ import {useEffect, useRef} from "preact/hooks";
 
 import style from './style.module.css';
 
-import {commonFields} from '../../../utils';
+import {commonFields, serialize} from '../../../utils';
 import ReadMore from '../../generic/read-more';
 import SuccessIndicator from '../../success-indicator';
 import {ExistingIcatEntity, IcatEntityValue, NewIcatEntity} from "../../../types";
 import {
     inIcatFormat,
     parseDate,
-    withCorrectedDateFormats
 } from "../../../dateUtils";
 import OnChangeInput from "../../generic/on-change-input";
 import {useMutation, UseMutationResult} from "@tanstack/react-query";
@@ -99,7 +98,7 @@ const EntityRow = (
         mutationFn: () => {
             // If entity.id is undefined, this is a new entity to be created
             // Otherwise we just want to send modifications with the current id
-            const e = withCorrectedDateFormats(isNewRow
+            const e = serialize(isNewRow
                 ? entity
                 : {...modifications, id: entity.id});
             return saveEntity(e);

src/utils.ts
@@ -1,10 +1,7 @@
-import dayjs from 'dayjs';
-import customParseFormat from 'dayjs/plugin/customParseFormat';
-
 import {entityNames} from './icat';
-import {TableFilter} from "./types";
+import {ExistingIcatEntity, NewIcatEntity, TableFilter} from "./types";
+import {inIcatFormat, parseDate} from "./dateUtils";
 
-dayjs.extend(customParseFormat);
 
 /**
  * Return the name of the entity which a one-many attribute points at.
@@ -221,4 +218,32 @@ export function range(a: number, b: number | null = null): number[] {
         ? a
         : b;
     return [...Array(top - bot).keys()].map(n => n + bot);
+}
+
+/**
+ * Convert an entity into a format that can be sent to ICAT
+ */
+export function serialize(entity: ExistingIcatEntity | NewIcatEntity) {
+    return Object.fromEntries(
+        Object.entries(entity)
+            .map(([k, v]) => {
+                if (typeof v === "object" || typeof v === "number") {
+                    return [k, v];
+                }
+
+                // Accepts date formats the don't specify milliseconds, and
+                // converts to one that does because ICAT requires it.
+                const asDate = parseDate(v);
+                if (asDate.isValid()) {
+                    return [k, inIcatFormat(asDate)];
+                }
+
+                if (v === "true") {
+                    return [k, true];
+                } else if (v === "false") {
+                    return [k, false];
+                }
+
+                return [k, v];
+            }));
 }

commit 7023672f45268e03bd7068d4463d7c703d86e882

Fix date formatting
Broken in fe7fa2efcde9c, when the custom format extension was removed rather than moved

src/dateUtils.ts
@@ -1,4 +1,7 @@
 import dayjs from "dayjs";
+import customParseFormat from 'dayjs/plugin/customParseFormat';
+
+dayjs.extend(customParseFormat);
 
 // Date formats that can be used for creating/editing date fields
 // Dates received from ICAT will always be in the first format
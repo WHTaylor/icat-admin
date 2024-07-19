e3100cb4830fa5bf2117458ab2a40f15085c1ca8
Will Taylor <william.h.taylor@stfc.ac.uk>
2024-07-19 15:57:27 +0100

    Update About page

--- src/components/about/index.tsx
+++ src/components/about/index.tsx
@@ -1,23 +1,27 @@
 const About = () => (
     <div class="page">
         <h2 class="leftColumn">About ICAT Admin</h2>
-        <p class="mainContent">A web app for browsing and editing ICAT data. Designed to be an alternative to <a
-            href="https://github.com/icatproject/manager.icat-manager">ICAT Manager</a>, which is no longer developed,
-            has some bugs, and is built on outdated dependencies.</p>
-        <p class="mainContent">The source code for the site is available <a
-            href="https://github.com/wonkyspecs/icat-admin">on github</a>. The site is built with <a
-            href="https://preactjs.com/">Preact</a>, and is currently hosted by <a
-            href="https://www.netlify.com/">Netlify</a>.</p>
-        <h3 class="leftColumn">Roadmap</h3>
-        <p class="mainContent">To reach feature parity with ICAT manager, the next set of features to add are:</p>
-        <ul class="mainContent">
-            <li>Validation and autofill during editing</li>
-        </ul>
-        <p class="mainContent">Additionally, I'm hoping to add:</p>
-        <ul class="mainContent">
-            <li>Changing column order/visibility</li>
-            <li>JPQL worksheets</li>
-        </ul>
+        <p class="mainContent">A web app for browsing and editing ICAT data.
+            Designed to be an alternative to
+            <a href="https://github.com/icatproject/manager.icat-manager">
+                ICAT Manager
+            </a>, which is no longer developed, has some bugs, and is built on
+            outdated dependencies.
+        </p>
+        <p class="mainContent">
+            The source code for the site is available <a
+            href="https://github.com/WHTaylor/icat-admin">on github</a>. The
+            site is built with <a href="https://preactjs.com/">Preact</a>, and is
+            currently hosted by <a href="https://www.netlify.com/">Netlify</a>.
+        </p>
+        <p class="mainContent">
+            If you have requests or come across problems with the site, please
+            open an <a href="https://github.com/WHTaylor/icat-admin/issues/new">
+            issue</a> or a <a
+            href="https://github.com/WHTaylor/icat-admin/discussions/new/choose">
+            discussion</a>, or post to the <a
+            href="https://groups.google.com/g/icatgroup">ICAT mailing list</a>
+        </p>
     </div>);
 
 export default About;
